c6c2939d833195ba0c87626013ff6de67305ed1a
Rajesh Sharma <rajeshsharma@rajesh.local>
2026-08-08 15:58:04 +1200

    updated code to calulate BMI using class and without inisilisation

--- BMI_Class.py
+++ BMI_Class.py
@@ -3,18 +3,24 @@ class BMICalculater:
     height = 0
     bmi = 0
 
-    def calculate_bmi():
-        bmi = weight / ((height**2) / 100)
-        bmi = round(bmi,2)
+    @staticmethod
+    def get_input():
+        BMICalculater.weight = float(input("Enter weight in kg: "))
+        BMICalculater.height = float(input("Enter height in cms: "))
 
+
+    @staticmethod
+    def calculate_bmi():
+        BMICalculater.height = BMICalculater.height / 100
+        BMICalculater.bmi = BMICalculater.weight / (BMICalculater.height**2)
+        BMICalculater.bmi = round(BMICalculater.bmi,2)
+
+    @staticmethod
     def display_bmi():
-        print(bmi)
+        print(BMICalculater.bmi)
 
 if __name__== "__main__":
-    user_weight = float(input("Enter weight: "))
-    user_height = float(input("Enter height: "))
     obj_bmi = BMICalculater()
-    obj_bmi.weight = user_weight
-    obj_bmi.height = user_height
+    obj_bmi.get_input()
     obj_bmi.calculate_bmi()
     obj_bmi.display_bmi()
